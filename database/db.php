a4804734dc84d83c50cfff0e3d2ac1b46a6be874
nikhilmukati <48977273+nikhilmukati@users.noreply.github.com>
2019-03-28 00:15:56 +0530

    db.php

---
+++ database/db.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database
+{
+    private $con;
+    public function connect(){
+        #include_once("constants.php);
+        $this->con=new Mysqli("HOST","USER","PASS","DB");
+        if($this->con)
+        {
+            echo "connected";
+            return $this->con;
+            
+        }else
+        {
+            return "DATABASE_CONNECTION_FAIL";
+        }
+    }
+}
+$db = new Database();
+$db->connect();
+?>
